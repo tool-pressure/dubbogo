40068dc7cb185a8204836867d4aacef46b873acd
alexstocks <dragoncharlie@foxmail.com>
2016-09-21 10:33:18 +0800

    init

---
+++ readme.md
@@ -0,0 +1,10 @@
+# dubbogo #
+a golang micro-service framework compatible with alibaba dubbo. just using jsonrpc 2.0 protocol over http now.
+
+## 说明 ##
+---
+> dubbogo 目前版本(0.1.1)支持的codec 是jsonrpc 2.0，transport protocol是http。
+> 只要你的java程序支持jsonrpc 2.0 over http，那么dubbogo程序就能调用它。
+> dubbogo自己的server端也已经实现，即dubbogo既能调用java service也能调用dubbogo实现的service。
+> 由于dubbogo还没有上传到github，使用的时候就放到路径$/gopath}/github.com/AlexStocks/下面。
+
---
+++ version/version.go
@@ -0,0 +1,19 @@
+/******************************************************
+# DESC    : dubbogo version
+# AUTHOR  : Alex Stocks
+# VERSION : 1.0
+# LICENCE : Apache Licence 2.0
+# EMAIL   : alexstocks@foxmail.com
+# MOD     : 2016-07-12 16:05
+# FILE    : version.go
+******************************************************/
+
+package version
+
+const (
+	// dubbogo version
+	Version       string = "0.1.1"
+	DUBBOGO_MAJOR        = 0
+	DUBBOGO_MINOR        = 1
+	DUBBOGO_BUILD        = 1
+)
